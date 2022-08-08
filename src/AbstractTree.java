d12c2cc1cb38292663f8d7b4d95a326c6b8d4955
yoshinkun2210 <yoshimkun22@gmail.com>
2022-08-08 23:49:08 +0700

    th

---
+++ src/AbstractTree.java
@@ -0,0 +1,6 @@
+public abstract class AbstractTree<E> implements Tree<E>{
+    @Override
+    /** Inorder traversal from the root*/
+    public void inorder() {
+    }
+}
---
+++ src/TreeNode.java
@@ -0,0 +1,10 @@
+
+public class TreeNode<E> {
+    protected E element;
+    protected TreeNode<E> left;
+    protected TreeNode<E> right;
+
+    public TreeNode(E e) {
+        element = e;
+    }
+}
